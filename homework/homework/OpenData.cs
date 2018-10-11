40c122f4129029446c024de38d5a7115be06786b
林其鴻 <h4568672@hotmail.com>
2018-10-11 20:57:21 +0800

    ADD

---
+++ homework/homework/OpenData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenDataImport.Models
+{
+    public class OpenData
+    {
+        //public int id { get; set; }
+
+        public string 地區 { get; set; }
+
+        public string 經度 { get; set; }
+
+        public string 緯度 { get; set; }
+    }
+}
---
+++ homework/homework/Program.cs
@@ -0,0 +1,69 @@
+﻿using OpenDataImport.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nodes = findOpenData();
+            ShowOpenData(nodes);
+            // Console.ReadKey();
+            Console.ReadLine();
+        }
+
+        static List<OpenData> findOpenData()
+        {
+            List<OpenData> result = new List<OpenData>();
+
+            var xml = XElement.Load(@"C:\Users\Henry\Downloads\O-A0001-001.xml");
+
+            XNamespace aw = "urn:cwb:gov:tw:cwbcommon:0.1";
+            var nodes = xml.Descendants(aw + "location").ToList();
+
+            for (var i = 0; i < nodes.Count; i++)
+            {
+                var node = nodes[i];
+
+                OpenData item = new OpenData();
+
+                //item.id = int.Parse(getValue(node, "id"));
+                item.地區 = getValue(node,aw + "locationName");
+                item.經度 = getValue(node,aw + "lat");
+                item.緯度= getValue(node,aw + "lon");
+                result.Add(item);
+            }
+            return result;
+        }
+
+        private static string getValue(XElement node, XName propertyName)
+        {
+            return node.Element(propertyName)?.Value?.Trim();
+        }
+
+        public static void ShowOpenData(List<OpenData> nodes)
+        {
+            Console.WriteLine(string.Format("共收到{0}筆的資料", nodes.Count));
+            /*nodes.GroupBy(node => node.地區).ToList()
+                 .ForEach(group =>
+                 {
+                     var key = group.Key;
+                     var groupDatas = group.ToList();
+                     var message = $"服務分類:{key},共有{groupDatas.Count()}筆資料";
+                     Console.WriteLine(message);
+                 });*/
+            for (var i = 0; i < nodes.Count; i++)
+            {
+                var node = nodes[i];
+                Console.WriteLine(string.Format("{0}.{1}", i + 1, node.地區));
+                Console.WriteLine(string.Format("\t經度:{0}", node.經度));
+                Console.WriteLine(string.Format("\t緯度:{0}", node.緯度));
+            }
+        }
+    }
+}
